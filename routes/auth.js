bd137c067b27c485c6dadf12d35ddf2b1aaffff4
gngsn <rudtjs4540@gmail.com>
2020-11-22 04:12:49 +0900

    update: fix conflict

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,11 @@ var router = express.Router();
 const { get, delByToken, checkRandToken } = require('../controllers/randToken');
 const { checkToken } = require('../middleware/auth')
 const { login, signup } = require('../controllers/user');
-const { signup, login } = require('../controllers/user');
 
 router.get('/rand-token', get);
 router.delete('/rand-token', checkToken, delByToken);
 router.post('/signup', signup);
 router.post('/login', login);
-
-
 router.get('/check-token', checkToken, checkRandToken);
 
 module.exports = router;
