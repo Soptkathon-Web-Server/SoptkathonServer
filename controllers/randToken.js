014c23b3227f326bdde40cc58ee733ed7bf0dd60
gngsn <rudtjs4540@gmail.com>
2020-11-22 01:01:56 +0900

    update: project setting

--- controllers/randToken.js
+++ controllers/randToken.js
@@ -3,8 +3,12 @@ const CODE = require('../modules/statusCode');
 const RES_MSSAGE = require('../modules/responseMessage');
 const encrypt = require('../modules/encrypt');
 const jwt = require('../modules/jwt');
+const crypto = require('crypto');
 
 module.exports = {
-    getRandToken: (req, res) => {
+    getRandToken: async (req, res) => {
+        
+        const dd = await crypto.randomBytes(4)
+        console.log(dd.toString('hex'));
     }
 }
